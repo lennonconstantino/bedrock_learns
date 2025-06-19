be9357cc66bc667486d0653f64d2db60f75f3d49
lennonconstantino <lennonconstantino@gmail.com>
2025-06-19 15:53:47 -0300

    Basic RAG app

---
+++ py/src/langchain/first_chain.py
@@ -0,0 +1,33 @@
+from langchain_aws import BedrockLLM as Bedrock
+from langchain_core.prompts import ChatPromptTemplate
+import boto3
+
+AWS_REGION = "us-east-1"
+
+bedrock = boto3.client(service_name="bedrock-runtime", region_name=AWS_REGION)
+
+model = Bedrock(model_id="amazon.titan-text-express-v1", client=bedrock)
+
+
+def invoke_model():
+    response = model.invoke("What is the highest mountain in the world?")
+    print(response)
+
+
+def fist_chain():
+    template = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "Write a short description for the product provided by the user",
+            ),
+            ("human", "{product_name}"),
+        ]
+    )
+    chain = template.pipe(model)
+
+    response = chain.invoke({"product_name": "bicycle"})
+    print(response)
+
+
+fist_chain()
---
+++ ts/src/langchain/BasicRag.ts
@@ -0,0 +1,60 @@
+import { BedrockEmbeddings } from "@langchain/aws";
+import { Bedrock } from "@langchain/community/llms/bedrock";
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
+import { Document } from '@langchain/core/documents'
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+
+const AWS_REGION = 'us-east-1'
+
+const model = new Bedrock({
+    model: 'amazon.titan-text-express-v1',
+    region: AWS_REGION
+})
+
+const myData = [
+    "The weather is nice today.",
+    "Last night's game ended in a tie.",
+    "Don likes to eat pizza.",
+    "Don likes to eat pasta.",
+    "Don likes to eat fruits",
+];
+
+const question = "What are Don's favorite foods?";
+
+async function main() {
+    const vectorStore = new MemoryVectorStore(
+        new BedrockEmbeddings({
+            region: AWS_REGION
+        })
+    );
+    await vectorStore.addDocuments(myData.map(
+        content => new Document({
+            pageContent: content
+        })
+    ))
+    const retriever = vectorStore.asRetriever({
+        k: 3
+    });
+    const results = await retriever.invoke(question);
+    const resultList = results.map(
+        result => result.pageContent
+    )
+
+    //build template:
+    const template = ChatPromptTemplate.fromMessages([
+        ['system', 'Answer the users question based on the following context: {context}'],
+        ['user', '{input}']
+    ]);
+
+    const chain = template.pipe(model);
+
+    const response = await chain.invoke({
+        input: question,
+        context: resultList
+    })
+
+    console.log(response)
+
+}
+
+main()
